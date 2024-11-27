9499df8e2f0600af5015c7a5cfbdc8ade9e76a4b
Maxdeoro <maxdeoro@gmail.com>
2024-11-27 23:20:48 +0300

    add index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Example AJAX</title>
+</head>
+<body>
+    Enter your name:
+    <input type="text" id="Name"/><br/>
+    <input type="button" value="Submit" name="mySubmit" onClick="ajax()"/>
+    <div id="OurMessage"></div>    <!-- accepts message from server -->
+
+    <script type="text/javascript" src="ajax.js"></script>
+</body>
+</html>
